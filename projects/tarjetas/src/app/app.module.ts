37b1d6bd78f64542a8e2ea103ba5473e914b262e
Roberto Garrido Trillo <robegt72@gmail.com>
2023-02-08 18:01:16 +0100

    initial commit

---
+++ projects/tarjetas/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CrearTarjetaComponent } from './components/crear-tarjeta/crear-tarjeta.component';
+import { ListarTarjetaComponent } from './components/listar-tarjeta/listar-tarjeta.component';
+import {ReactiveFormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CrearTarjetaComponent,
+    ListarTarjetaComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
